cd57fd6e54ae62577777296fff6c68cf61e3827d
xushengdong <shengdong.xu@hairoutech.com>
2018-03-07 17:05:54 +0800

    work for higher grade

--- coloring/solver.py
+++ coloring/solver.py
@@ -56,7 +56,7 @@ def solve_it(input_data):
             if node_temp.edges.count(-1) > best_count:
                 best_count = node_temp.edges.count(-1)
                 best_index_2 = j
-        if best_index_1 > 0 and len(nodes[best_index_2].domain) > 0:
+        if best_index_1 > 0 and (len(nodes[best_index_2].domain) > 0 or best_index_2 < 0) > 0:
             best_index = best_index_1
         else:
             best_index = best_index_2
@@ -77,8 +77,21 @@ def solve_it(input_data):
                     nodes[q].domain.append(color_count)
             color_count += 1
         else:
-            min_influence_count = node_count - i + 1
-            best_color_choice = 0
+            # best_count = node_count
+            # best_index_2 = -1
+            # for j in range(i, node_count):
+            #     if node_temp.edges.count(-1) < best_count:
+            #         best_count = node_temp.edges.count(-1)
+            #         best_index_2 = j
+            #
+            # node_pop = nodes.pop(best_index_2)
+            # nodes.insert(i, node_pop)
+            # for node in nodes:
+            #     pop_element = node.edges.pop(best_index_2)
+            #     node.edges.insert(i, pop_element)
+
+            min_influence_count = node_count
+            best_color_choice = -1
             for color_choice_i in range(len(nodes[i].domain)):
                 influence_count = 0
                 for j in range(i, node_count):
@@ -102,6 +115,10 @@ def solve_it(input_data):
                             index_pop = index
                             break
                     nodes[q].domain.pop(index_pop)
+
+    solution_greedy_raw = [0 for i in range(node_count)]
+    for i in range(len(nodes)):
+        solution_greedy_raw[i] = nodes[i].color[0]
 
     for node in nodes:
         solution_greedy[node.index] = node.color[0]
@@ -129,28 +146,53 @@ def solve_it(input_data):
                          solution_matrix[i], count)
         nodes_start[i] = node_temp
 
-    Solution = namedtuple('Solution', ['color_number', 'depth', 'nodes'])
+    Solution = namedtuple('Solution', ['color_number', 'depth', 'nodes', 'active_add_count'])
 
-    solution_root = Solution(-1, 0, [node for node in nodes_start])
-    solution_sets = [solution_root]
-    current_opt = greedy_opt - 1
+    solution_root = Solution(-1, 0, [node for node in nodes_start], 0)
+    solution_sets = []
+    solution_sets.append(solution_root)
+    current_opt = (greedy_opt - 1)
     best_solution_nodes = greedy_solution_nodes
+
+    test_count = 0
+    # current_time = datetime.now()
+    # timespan = current_time - start
+    # print('Time: ' + str(timespan.total_seconds()))
+    # print('Find: ' + str(greedy_opt) + ' ' + str(solution_greedy_raw))
+    opt_ratio = 1
+    if node_count < 101:
+        exit_time = 100000000000
+    else:
+        exit_time = 360
     while len(solution_sets):
         current_time = datetime.now()
         timespan = current_time - start
-        if timespan.total_seconds() >= 360:
+        if timespan.total_seconds() >= exit_time:
+            opt_ratio = 0
 #            print('Time excessed')
             break
-        solution_sets.sort(key=lambda x: -(x[1] - x[0]))
+        test_count += 1
+
         solution_sets.sort(key=lambda x: x[0])
+        solution_sets.sort(key=lambda x: -x[3])
+        solution_sets.sort(key=lambda x: (x[1] - x[0]))
 
         current_solution = solution_sets.pop()
         current_depth = current_solution.depth
         current_color_number = current_solution.color_number
         current_nodes = current_solution.nodes
+        current_active_add_count = current_solution.active_add_count
+#        print(str(current_depth) + ' ' + str(current_color_number + 1))
         if current_depth == node_count and current_color_number < current_opt:
             current_opt = current_solution.color_number
             best_solution_nodes = current_solution.nodes
+            # current_time = datetime.now()
+            # timespan = current_time - start
+            # solution_temp = [0 for i in range(node_count)]
+            # for i in range(len(best_solution_nodes)):
+            #     solution_temp[i] = best_solution_nodes[i].color[0]
+            # print('Time: ' + str(timespan.total_seconds()))
+            # print('Find: ' + str(current_opt + 1) + ' ' + str(solution_temp))
         if current_depth < node_count and current_color_number < current_opt:
             best_count = -1
             best_index_1 = -1
@@ -162,7 +204,7 @@ def solve_it(input_data):
                 if node_temp.edges.count(-1) > best_count:
                     best_count = node_temp.edges.count(-1)
                     best_index_2 = j
-            if best_index_1 > 0 and len(current_nodes[best_index_2].domain) > 0:
+            if best_index_1 > 0 and (len(current_nodes[best_index_2].domain) > 0 or best_index_2 < 0) > 0:
                 best_index = best_index_1
             else:
                 best_index = best_index_2
@@ -186,7 +228,7 @@ def solve_it(input_data):
                 color_lists[current_depth].append(current_color_number)
                 #                current_nodes[current_depth].color.append(current_color_number)
                 for t in range(current_depth + 1):
-                    if current_color_number != current_nodes[t].color:
+                    if current_color_number != color_lists[t][0]:
                         current_nodes[t].edges[current_depth] = node_count
                         current_nodes[current_depth].edges[t] = node_count
                 for q in range(current_depth + 1, node_count):
@@ -198,45 +240,149 @@ def solve_it(input_data):
                     temp_nodes.append(Node(current_nodes[i].index, current_nodes[i].rank,
                                            color_lists[i], current_nodes[i].domain, current_nodes[i].edges,
                                            current_nodes[i].count))
-                solution_new = Solution(current_color_number, current_depth + 1, temp_nodes)
+                solution_new = Solution(current_color_number, current_depth + 1, temp_nodes, current_active_add_count)
                 solution_sets.append(solution_new)
             else:
-                for d_index in range(len(current_nodes[current_depth].domain)):
-                    solution_matrix_temp = [[0 for i in range(node_count)] for j in range(node_count)]
-                    domain_lists = [[] for i in range(node_count)]
+                best_node_choice = -1
+                best_color_index = -1
+                for t_1 in range(current_depth, node_count):
+                    current_node_domain = current_nodes[t_1].domain
+                    best_color_index = -1
+                    for choice_color_i in range(len(current_node_domain)):
+                        check_1 = 1
+                        for t_2 in range(current_depth, node_count):
+                            if current_node_domain[choice_color_i] in current_nodes[t_2].domain and current_nodes[t_2].edges[t_1] == -1:
+                                check_1 = 0
+                                break
+                        if check_1 == 1:
+                            best_color_index = choice_color_i
+                            break
+                    if best_color_index > 0:
+                        best_node_choice = t_1
+                        break
+
+                if best_node_choice > 0 and best_color_index > 0:
+                    permute_node = current_nodes.pop(best_node_choice)
+                    current_nodes.insert(current_depth, permute_node)
+                    for node in current_nodes:
+                        pop_element = node.edges.pop(best_node_choice)
+                        node.edges.insert(current_depth, pop_element)
+                    color_temp = current_nodes[current_depth].domain[best_color_index]
+                    while len(current_nodes[current_depth].domain):
+                        current_nodes[current_depth].domain.pop()
+                    current_nodes[current_depth].color.append(color_temp)
+                    for t in range(current_depth + 1):
+                        if current_color_number != current_nodes[t].color[0]:
+                            current_nodes[t].edges[current_depth] = node_count
+                            current_nodes[current_depth].edges[t] = node_count
+                    solution_new = Solution(current_color_number, current_depth + 1, current_nodes, current_active_add_count)
+                    solution_sets.append(solution_new)
+                else:
+                    list_t = [[] for i in range(len(current_nodes[current_depth].domain))]
+                    for d_index in range(len(current_nodes[current_depth].domain)):
+                        test_list = [0 for i in range(node_count - current_depth)]
+                        for i in range(current_depth, node_count):
+                            if current_nodes[current_depth].domain[d_index] in current_nodes[i].domain and current_nodes[current_depth].edges[i] == -1:
+                                test_list[i - current_depth] = len(current_nodes[i].domain) - current_nodes[i].domain.count(current_nodes[current_depth].domain[d_index])
+                                list_t[d_index].append(len(current_nodes[i].domain) - 1)
+                            else:
+                                test_list[i - current_depth] = len(current_nodes[i].domain)
+                                list_t[d_index].append(len(current_nodes[i].domain))
+                    # for i in range(len(list_t)):
+                    #     print(list_t[i])
+                    compare_result = [0 for i in range(len(current_nodes[current_depth].domain))]
+                    for i in range(len(list_t)):
+                        for j in range(i, len(list_t)):
+                            compare_list = list_t[i]
+                            compared_list = list_t[j]
+                            count_1 = 0
+                            count_2 = 0
+                            for p in range(len(compare_list)):
+                                if compare_list[p] > compared_list[p]:
+                                    count_1 += 1
+                                if compare_list[p] < compared_list[p]:
+                                    count_2 += 1
+                            if count_1 == 0 and count_2 >= 0:
+                                compare_result[i] = j
+                                compare_result[j] = j
+                            elif count_2 == 0 and count_1 >= 0:
+                                compare_result[i] = i
+                                compare_result[j] = i
+                    # print('Compare_result: ' + str(compare_result))
+                    # print(current_nodes[current_depth].domain)
+                    pop_count = 0
+                    for i in range(len(compare_result)):
+                        if compare_result[i] != i:
+                            current_nodes[current_depth].domain.pop(i - pop_count)
+                            pop_count += 1
+                    # print(current_nodes[current_depth].domain)
+                    # print('End')
+                    test_count += 1
+                    # if test_count > 1000:
+                    #     exit(1)
+
+                    for d_index in range(len(current_nodes[current_depth].domain)):
+                        solution_matrix_temp = [[0 for i in range(node_count)] for j in range(node_count)]
+                        domain_lists = [[] for i in range(node_count)]
+                        color_lists = [[] for i in range(node_count)]
+                        for i in range(node_count):
+                            for j in range(node_count):
+                                solution_matrix_temp[i][j] = current_nodes[i].edges[j]
+                            for index_x in range(len(current_nodes[i].domain)):
+                                domain_lists[i].append(current_nodes[i].domain[index_x])
+                            for index_x in range(len(current_nodes[i].color)):
+                                color_lists[i].append(current_nodes[i].color[index_x])
+
+                        result_lists = [[] for i in range(node_count)]
+                        color_temp = domain_lists[current_depth][d_index]
+
+                        for t_index in range(len(domain_lists[current_depth])):
+                            if t_index != d_index:
+                                result_lists[current_depth].append(domain_lists[t_index])
+
+                        color_lists[current_depth].append(color_temp)
+
+                        for t in range(current_depth + 1):
+                            if color_temp != color_lists[t][0]:
+                                solution_matrix_temp[t][current_depth] = node_count
+                                solution_matrix_temp[current_depth][t] = node_count
+                        for q in range(current_depth + 1, node_count):
+                            for index in range(len(domain_lists[q])):
+                                if domain_lists[q][index] != color_temp or solution_matrix_temp[q][current_depth] != -1:
+                                    result_lists[q].append(domain_lists[q][index])
+                        temp_nodes = []
+                        for i in range(node_count):
+                            temp_nodes.append(Node(current_nodes[i].index, current_nodes[i].rank,
+                                                   color_lists[i], result_lists[i], solution_matrix_temp[i],
+                                                   current_nodes[i].count))
+                        solution_new = Solution(current_color_number, current_depth + 1, temp_nodes, current_active_add_count)
+                        solution_sets.append(solution_new)
+                    current_color_number += 1
                     color_lists = [[] for i in range(node_count)]
                     for i in range(node_count):
-                        for j in range(node_count):
-                            solution_matrix_temp[i][j] = current_nodes[i].edges[j]
-                        for index_x in range(len(current_nodes[i].domain)):
-                            domain_lists[i].append(current_nodes[i].domain[index_x])
                         for index_x in range(len(current_nodes[i].color)):
                             color_lists[i].append(current_nodes[i].color[index_x])
-
-                    result_lists = [[] for i in range(node_count)]
-                    color_temp = domain_lists[current_depth][d_index]
-
-                    for t_index in range(len(domain_lists[current_depth])):
-                        if t_index != d_index:
-                            result_lists[current_depth].append(domain_lists[t_index])
-
-                    color_lists[current_depth].append(color_temp)
-
+                    color_lists[current_depth].append(current_color_number)
+                    #                current_nodes[current_depth].color.append(current_color_number)
                     for t in range(current_depth + 1):
-                        if color_temp != color_lists[t][0]:
-                            solution_matrix_temp[t][current_depth] = node_count
-                            solution_matrix_temp[current_depth][t] = node_count
+                        if current_color_number != color_lists[t][0]:
+                            current_nodes[t].edges[current_depth] = node_count
+                            current_nodes[current_depth].edges[t] = node_count
                     for q in range(current_depth + 1, node_count):
-                        for index in range(len(domain_lists[q])):
-                            if domain_lists[q][index] != color_temp or solution_matrix_temp[q][current_depth] != -1:
-                                result_lists[q].append(domain_lists[q][index])
+                        if current_color_number not in current_nodes[q].domain \
+                                and current_nodes[q].edges[current_depth] != -1:
+                            current_nodes[q].domain.append(current_color_number)
                     temp_nodes = []
                     for i in range(node_count):
                         temp_nodes.append(Node(current_nodes[i].index, current_nodes[i].rank,
-                                               color_lists[i], result_lists[i], solution_matrix_temp[i],
+                                               color_lists[i], current_nodes[i].domain, current_nodes[i].edges,
                                                current_nodes[i].count))
-                    solution_new = Solution(current_color_number, current_depth + 1, temp_nodes)
+                    solution_new = Solution(current_color_number, current_depth + 1, temp_nodes, current_active_add_count + 1)
                     solution_sets.append(solution_new)
+
+    solution_raw = [0 for i in range(node_count)]
+    for i in range(len(best_solution_nodes)):
+        solution_raw[i] = best_solution_nodes[i].color[0]
 
     for node in best_solution_nodes:
 #        print(node)
@@ -245,19 +391,24 @@ def solve_it(input_data):
         else:
             solution_greedy[node.index] = -1
     current_opt += 1
-#    print(str(greedy_opt) + ' -> ' + str(current_opt))
+
     color_count = current_opt
     solution = solution_greedy
     for edge in edges:
         if solution_greedy[edge[0]] == solution_greedy[edge[1]]:
             print('XXX')
     # prepare the solution in the specified output format
-    output_data = str(color_count) + ' ' + str(0) + '\n'
+    output_data = str(color_count) + ' ' + str(opt_ratio) + '\n'
     output_data += ' '.join(map(str, solution))
-    print(output_data)
-    current_time = datetime.now()
-    timespan = current_time - start
-    print('Time: ' + str(timespan.total_seconds()))
+
+#    print(solution_greedy_raw)
+#    print(solution_raw)
+#     print(test_count)
+#     print(str(greedy_opt) + ' -> ' + str(current_opt))
+#     print(output_data)
+#     current_time = datetime.now()
+#     timespan = current_time - start
+#     print('Time: ' + str(timespan.total_seconds()))
 
     return output_data
 
