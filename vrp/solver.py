18fb3b6ab0260b8d19fd19f7d5c14fa010c4bf87
xushengdong <shengdong.xu@hairoutech.com>
2018-04-18 08:27:02 +0800

    pass vrp baseline

--- vrp/solver.py
+++ vrp/solver.py
@@ -14,6 +14,8 @@ Edge = namedtuple("Edge", ['length', 'start', 'end', 'index'])
 
 Customer = namedtuple("Customer", ['index', 'demand', 'x', 'y'])
 
+PartialSolution = namedtuple("PartialSolution", ['score', 'customers'])
+
 def length(customer1, customer2):
     return math.sqrt((customer1.x - customer2.x)**2 + (customer1.y - customer2.y)**2)
 
@@ -49,6 +51,7 @@ def solve_it(input_data):
         # print "Start Vehicle: ",v
         vehicle_tours.append([])
         capacity_remaining = vehicle_capacity
+        # print(remaining_customers)
         while sum([capacity_remaining >= customer.demand for customer in remaining_customers]) > 0:
             used = set()
             order = sorted(remaining_customers, key=lambda customer: -customer.demand)
@@ -59,6 +62,7 @@ def solve_it(input_data):
                     # print '   add', ci, capacity_remaining
                     used.add(customer)
             remaining_customers -= used
+
 
     # checks that the number of customers served is correct
     assert sum([len(v) for v in vehicle_tours]) == len(customers) - 1
@@ -102,12 +106,185 @@ def solve_it(input_data):
         if len(vehicle_tour) > 0:
             obj += length(depot, vehicle_tour[0])
             for i in range(0, len(vehicle_tour)-1):
-                obj += length(vehicle_tour[i],vehicle_tour[i+1])
-            obj += length(vehicle_tour[-1],depot)
+                obj += length(vehicle_tour[i], vehicle_tour[i+1])
+            obj += length(vehicle_tour[-1], depot)
+
+    obj_curr_opt = obj
+    vehicles_tours_curr_opt = vehicle_tours
+
+    Solution_set = []
+    tabu_list = [[] for v in range(0, vehicle_count)]
+
+    for v in range(0, vehicle_count):
+        customer_list = []
+        vehicle_tour = vehicle_tours[v]
+        if len(vehicle_tour) > 0:
+            for i in range(0, len(vehicle_tour)-1):
+                customer_list.append(vehicle_tour[i].index)
+            customer_list.append(vehicle_tour[-1].index)
+        tabu_list[v].append(customer_list)
+    test_list = [customer.index for customer in vehicle_tours[0]]
+    test_list.sort()
+    # print(test_list)
+    for t in tabu_list:
+        # print(t)
+        for el in t:
+            el.sort()
+    # if test_list in tabu_list[0]:
+    #     print('yes')
+    #     # print(tabu_list[0])
+    # else:
+    #     print('no')
+    #     print(tabu_list[0])
+    if customer_count < 150:
+        factor = 0.01
+    else:
+        factor = 0.001
+    iter_count = 200000
+    test_count = 0
+    while iter_count:
+        iter_count -= 1
+        exit_column = random.randint(0, vehicle_count - 1)
+        enter_column = random.randint(0, vehicle_count - 1)
+        if exit_column != enter_column and len(vehicle_tours[exit_column]) != 0:
+            exit_row = random.randint(0, len(vehicle_tours[exit_column]) - 1)
+            tmp_list_exit = [customer.index for customer in vehicle_tours[exit_column]]
+            # print(tmp_list_exit)
+            exit_customer_index = vehicle_tours[exit_column][exit_row].index
+            tmp_list_exit.pop(exit_row)
+            tmp_list_exit.sort()
+
+
+            # print(str(exit_column) + ' ' + str(exit_row))
+
+            # print(exit_customer_index)
+            # print(tmp_list_exit)
+
+            tmp_list_enter = [customer.index for customer in vehicle_tours[enter_column]]
+            tmp_list_enter.sort()
+            # print(tmp_list_enter)
+            tmp_list_enter.append(exit_customer_index)
+            tmp_list_enter.sort()
+            # print(tmp_list_enter)
+            # print('## Tabu List Start:##')
+            # for t in tabu_list:
+            #     print(t)
+            # print('## Tabu List End   ##')
+            enter_capacity = sum([customers[i].demand for i in tmp_list_enter])
+            if enter_capacity >= vehicle_capacity:
+                switch_row = random.randint(0, len(tmp_list_enter) - 1)
+                exit_element = tmp_list_enter.pop(switch_row)
+                tmp_list_exit.append(exit_element)
+                tmp_list_exit.sort()
+
+            enter_capacity_1 = sum([customers[i].demand for i in tmp_list_enter])
+            enter_capacity_2 = sum([customers[i].demand for i in tmp_list_exit])
+            if enter_capacity_1 >= vehicle_capacity or enter_capacity_2 >= vehicle_capacity:
+                # print("Not Enough and Continue")
+                continue
+
+            if tmp_list_enter not in tabu_list[enter_column] or tmp_list_exit not in tabu_list[exit_column] or True:
+                tabu_list[enter_column].append(tmp_list_enter)
+                tabu_list[exit_column].append(tmp_list_exit)
+                test_count += 1
+
+                index_list = [i for i in tmp_list_enter]
+                inputData = str(len(index_list) + 1) + '\n'
+                inputData += str(depot.x) + ' ' + str(depot.y) + '\n'
+                for index in index_list:
+                    inputData += str(customers[index].x) + ' ' + str(customers[index].y) + '\n'
+                swap_list = tsp_solver(inputData)
+                # print(swap_list)
+                while True:
+                    if swap_list[0] != 0:
+                        swap_element = swap_list.pop(0)
+                        swap_list.append(swap_element)
+                    else:
+                        # swap_list.pop(0)
+                        break
+                swap_list.pop(0)
+                vehicle_tour_new_1 = []
+                for index in swap_list:
+                    vehicle_tour_new_1.append(customers[tmp_list_enter[index -1]])
+
+
+                index_list = [i for i in tmp_list_exit]
+                inputData = str(len(index_list) + 1) + '\n'
+                inputData += str(depot.x) + ' ' + str(depot.y) + '\n'
+                for index in index_list:
+                    inputData += str(customers[index].x) + ' ' + str(customers[index].y) + '\n'
+                swap_list = tsp_solver(inputData)
+                # print(swap_list)
+                while True:
+                    if swap_list[0] != 0:
+                        swap_element = swap_list.pop(0)
+                        swap_list.append(swap_element)
+                    else:
+                        # swap_list.pop(0)
+                        break
+                swap_list.pop(0)
+                vehicle_tour_new_2 = []
+                for index in swap_list:
+                    vehicle_tour_new_2.append(customers[tmp_list_exit[index - 1]])
+
+                obj_temp = 0
+                for v in range(0, vehicle_count):
+                    if v == exit_column:
+                        vehicle_tour = vehicle_tour_new_2
+                    elif v == enter_column:
+                        vehicle_tour = vehicle_tour_new_1
+                    else:
+                        vehicle_tour = vehicle_tours[v]
+                    if len(vehicle_tour) > 0:
+                        obj_temp += length(depot, vehicle_tour[0])
+                        for i in range(0, len(vehicle_tour) - 1):
+                            obj_temp += length(vehicle_tour[i], vehicle_tour[i + 1])
+                        obj_temp += length(vehicle_tour[-1], depot)
+                if obj_temp < obj_curr_opt:
+                    # print('Achieve Score: ' + str(obj_temp))
+                    epselon = obj_curr_opt - obj_temp
+                    obj_curr_opt = obj_temp
+                    temp_tour = [tour for tour in vehicle_tours]
+
+                    while len(temp_tour[exit_column]) != 0:
+                        temp_tour[exit_column].pop(-1)
+                    for v in vehicle_tour_new_2:
+                        temp_tour[exit_column].append(v)
+
+                    while len(temp_tour[enter_column]) != 0:
+                        temp_tour[enter_column].pop(-1)
+                    for v in vehicle_tour_new_1:
+                        temp_tour[enter_column].append(v)
+                    vehicles_tours_curr_opt = temp_tour
+                    if epselon/obj_curr_opt > 0.0:
+                        vehicle_tours = temp_tour
+
+                else:
+                    epselon = obj_temp - obj_curr_opt
+                    if epselon/obj_curr_opt <= factor:
+                        while len(vehicle_tours[exit_column]) != 0:
+                            vehicle_tours[exit_column].pop(-1)
+                        for v in vehicle_tour_new_2:
+                            vehicle_tours[exit_column].append(v)
+
+                        while len(vehicle_tours[enter_column]) != 0:
+                            vehicle_tours[enter_column].pop(-1)
+                        for v in vehicle_tour_new_1:
+                            vehicle_tours[enter_column].append(v)
+
+            # else:
+            #     print('already in tabu list')
+
+    # print(test_count)
+
+    obj = obj_curr_opt
+    vehicle_tours = vehicles_tours_curr_opt
+
     # prepare the solution in the specified output format
     outputData = '%.2f' % obj + ' ' + str(0) + '\n'
     for v in range(0, vehicle_count):
         outputData += str(depot.index) + ' ' + ' '.join([str(customer.index) for customer in vehicle_tours[v]]) + ' ' + str(depot.index) + '\n'
+
 
     for vehicle_tour in vehicle_tours:
         total_demand = 0
@@ -133,12 +310,9 @@ def solve_it(input_data):
     #         for i in range(0, len(vehicle_tour)-1):
     #             plt.plot([vehicle_tour[i].x, vehicle_tour[i+1].x], [vehicle_tour[i].y, vehicle_tour[i+1].y], color=color_list[v%len(color_list)])
     #         plt.plot([vehicle_tour[len(vehicle_tour)-1].x, depot.x], [vehicle_tour[len(vehicle_tour)-1].y, depot.y], color=color_list[v%len(color_list)])
-    # print(valid_count)
-    # for i in range(-1, nodeCount - 1):
-    #     plt.plot([points[solution_opt[i]].x, points[solution_opt[i + 1]].x],
-    #              [points[solution_opt[i]].y, points[solution_opt[i + 1]].y])
     # plt.legend()
-    plt.show()
+    # plt.show()
+
     # print(outputData)
     return outputData
 
@@ -346,9 +520,9 @@ def tsp_solver(input_data):
         obj_new = length(points[solution_new[-1]], points[solution_new[0]])
         for index in range(0, nodeCount - 1):
             obj_new += length(points[solution_new[index]], points[solution_new[index + 1]])
-
-
     return solution_opt
+
+
 import sys
 
 if __name__ == '__main__':
@@ -359,17 +533,17 @@ if __name__ == '__main__':
             input_data = input_data_file.read()
         solve_it(input_data)
     else:
-        print(
-            'This test requires an input file.  Please select one from the data directory. (i.e. python solver.py ./data/tsp_51_1)')
-        # file_path_1 = '/home/kaylor/Desktop/discrete_optimization/vrp/data/vrp_16_3_1'
-        # file_path_2 = '/home/kaylor/Desktop/discrete_optimization/vrp/data/vrp_26_8_1'c
-        # file_path_3 = '/home/kaylor/Desktop/discrete_optimization/vrp/data/vrp_51_5_1'
-        # file_path_4 = '/home/kaylor/Desktop/discrete_optimization/vrp/data/vrp_101_10_1'
-        # file_path_5 = '/home/kaylor/Desktop/discrete_optimization/vrp/data/vrp_200_16_1'
-        # file_path_6 = '/home/kaylor/Desktop/discrete_optimization/vrp/data/vrp_421_41_1'
-        # file_location = file_path_1.strip()
-        #
-        # with open(file_location, 'r') as input_data_file:
-        #     input_data = input_data_file.read()
-        # solve_it(input_data)
+        # print(
+        #     'This test requires an input file.  Please select one from the data directory. (i.e. python solver.py ./data/tsp_51_1)')
+        file_path_1 = '/home/kaylor/Desktop/discrete_optimization/vrp/data/vrp_16_3_1'
+        file_path_2 = '/home/kaylor/Desktop/discrete_optimization/vrp/data/vrp_26_8_1'
+        file_path_3 = '/home/kaylor/Desktop/discrete_optimization/vrp/data/vrp_51_5_1'
+        file_path_4 = '/home/kaylor/Desktop/discrete_optimization/vrp/data/vrp_101_10_1'
+        file_path_5 = '/home/kaylor/Desktop/discrete_optimization/vrp/data/vrp_200_16_1'
+        file_path_6 = '/home/kaylor/Desktop/discrete_optimization/vrp/data/vrp_421_41_1'
+        file_location = file_path_6.strip()
+
+        with open(file_location, 'r') as input_data_file:
+            input_data = input_data_file.read()
+        solve_it(input_data)
 
